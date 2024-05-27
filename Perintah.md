dd254f480444f6e6bae9b358f5854761e523be94
Masrianto <45374438+masriant@users.noreply.github.com>
2024-05-28 05:21:10 +0700

    Post Database

--- Perintah.md
+++ Perintah.md
@@ -1,9 +1,18 @@
-<!-- Jalankan -->
+<!-- Start -->
 npm run dev
 
-<!--  -->
+php artisan
+<!-- View - Component -->
 php artisan make:component Navbar
 {{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}
 php artisan make:component Header --view
 php artisan make:component Layout
 php artisan make:component NavLink --view
+
+<!-- Database -->
+php artisan migrate
+php artisan migrate:fresh
+<!-- Buat database -->
+php artisan make:migration > create_posts_table
+
+php artisan tinker
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,38 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Masrianto',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sed modi, reprehenderit nesciunt placeat consequuntur pariatur doloremque aperiam temporibus ut labore ad tempora adipisci quidem voluptatibus, optio cumque ullam eveniet sint, deserunt nobis cum. Ex saepe quaerat architecto velit porro neque, nobis dicta harum, tempore cum dolor nemo at voluptatum!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Masrianto',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur numquam adipisci, ullam corrupti laudantium magnam totam veniam id, voluptatum velit commodi eveniet alias repudiandae odio mollitia facere incidunt optio, dolorum distinctio. Velit deleniti fugit est repellat debitis sapiente, doloribus corrupti ex dolore culpa voluptas, magni minima! Soluta, distinctio, ex alias minus doloremque accusamus veniam ullam magnam facere tempora laboriosam. Ratione fugiat dolor ipsa. Saepe praesentium, ad rem odit temporibus aspernatur.!'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
