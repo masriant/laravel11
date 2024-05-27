5f62f968c512102ac0ac6530dcf1956046f476eb
Masrianto <45374438+masriant@users.noreply.github.com>
2024-05-28 02:15:53 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,6 +28,12 @@ class Post
 
     public static function find($slug): array
     {
-        return Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+
+        if (!$post) {
+            abort(404);
+        }
+
+        return $post;
     }
 }
