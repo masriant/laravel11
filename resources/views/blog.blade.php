16b521a657e22e63a04e5137fa5e37088482e0b3
Masrianto <45374438+masriant@users.noreply.github.com>
2024-05-27 21:32:38 +0700

    Text Blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,20 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Ini adalah halaman Blog Page</h3>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Masrianto</a> | 27 May 2024
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sed modi, reprehenderit nesciunt placeat consequuntur pariatur doloremque aperiam temporibus ut labore ad tempora adipisci quidem voluptatibus, optio cumque ullam eveniet sint, deserunt nobis cum. Ex saepe quaerat architecto velit porro neque, nobis dicta harum, tempore cum dolor nemo at voluptatum!</p>
+        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Masrianto</a> | 28 May 2024
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur numquam adipisci, ullam corrupti laudantium magnam totam veniam id, voluptatum velit commodi eveniet alias repudiandae odio mollitia facere incidunt optio, dolorum distinctio. Velit deleniti fugit est repellat debitis sapiente, doloribus corrupti ex dolore culpa voluptas, magni minima! Soluta, distinctio, ex alias minus doloremque accusamus veniam ullam magnam facere tempora laboriosam. Ratione fugiat dolor ipsa. Saepe praesentium, ad rem odit temporibus aspernatur.</p>
+        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
 </x-layout>
